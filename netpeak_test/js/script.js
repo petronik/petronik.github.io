985d45c371709af352c81be8cbd59e0c7879563a
Petro_Nikolaiev <petronik@ukr.net>
2018-07-09 00:20:41 +0300

    added animation

--- netpeak_test/js/script.js
+++ netpeak_test/js/script.js
@@ -12,3 +12,13 @@ $('a').hover(function(){
     $(this).css("transform", "scale(1)");
 
 });
+$('.btn1').mouseenter(function () {
+    $(this).css({"transition": "0.5s"});
+    $(this).css({"transform": "scale(1.2)"})
+}).mouseleave(function () {
+    $(this).css("transform", "scale(1)")
+
+});
+$('.container-3__item_box').on('click', function(){
+    $('.item1').toggleClass('item1-origin');
+});
