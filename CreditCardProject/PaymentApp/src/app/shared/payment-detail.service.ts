88fc418849aba56e824bfa730876a100bdbad073
Petronik <petronik@ukr.net>
2021-09-18 07:34:04 -0500

    initial commit

---
+++ CreditCardProject/PaymentApp/src/app/shared/payment-detail.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { PaymentDetail } from './payment-detail.model';
+import {HttpClient} from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaymentDetailService {
+
+  constructor(private http:HttpClient) { }
+
+formData:PaymentDetail = new PaymentDetail();
+readonly baseUrl:"https://localhost:44374/api/PaymentDetails"
+
+postPaymentDetail(){
+  return this.http.post(this.baseUrl, this.formData)
+}
+}
